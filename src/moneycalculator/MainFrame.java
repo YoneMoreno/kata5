e6e2f0f773d6672f3537a11cdef2dfbc77ab1f16
usuario <usuario@10.22.143.48>
2016-11-10 12:37:28 +0100

    Kata 5 MoneyCalculator v2 Se usa swing para crear una ventana, una barra de menús, un menú, su elemento menú interno e imprimimos al clicar en él "Calculate". Añadido 4 botones en disposición bordes, norte, este oeste sur. Comentado añadido de un "Muestra Valor Divisa" debido a duda: ¿Por qué Swing[...] devuelve un Swing[...] cuando hemos puesto en la cabecera que devuelve JPanel. Aprenndimos CTRL+B en un método observas el nivel inferior el cuerpo del mismo Aprendimos, también: ALT+(cursor DCHA o IZQD) observas el nivel superior de donde se viene

--- src/moneycalculator/MainFrame.java
+++ src/moneycalculator/MainFrame.java
@@ -1,15 +1,23 @@
 package moneycalculator;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Label;
 import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
+import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
+    private int index;
     public MainFrame(){
         setTitle("money Calculator");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -17,6 +25,12 @@ public class MainFrame extends JFrame {
         setMinimumSize(new Dimension(400,400));
         setLocationRelativeTo(null);
         this.setMenuBar(menuBar());
+        setLayout(new BorderLayout());
+        add(label(), BorderLayout.NORTH);
+        add(label(), BorderLayout.SOUTH);
+        add(label(), BorderLayout.WEST);
+        add(label(), BorderLayout.EAST);
+        //add(moneyDisplay());
         setVisible(true);
     }
 
@@ -48,4 +62,20 @@ public class MainFrame extends JFrame {
             }
         };
     }
+
+    private JPanel label() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        panel.add(new JLabel("A" + index++));
+        return panel;
+    }
+/*
+    private JPanel moneyDisplay() {
+        SwingMoneyDisplay swingMoneyDisplay = new SwingMoneyDisplay();
+        return swingMoneyDisplay;
+    }
+    */
+
+    
+ 
 }
---
+++ src/moneycalculator/SwingMoneyDisplay.java
@@ -0,0 +1,25 @@
+package moneycalculator;
+
+import java.awt.FlowLayout;
+import java.awt.PopupMenu;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SwingMoneyDisplay extends JPanel implements MoneyDisplay {
+
+    public SwingMoneyDisplay() {
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        add(amount());
+        add(currency());
+    }
+
+    private JLabel amount() {
+        return new JLabel("$");
+    }
+
+    private JLabel currency() {
+        return new JLabel("500");
+    }
+    
+    
+}
